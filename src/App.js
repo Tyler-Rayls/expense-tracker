cce67641c548075c821238cb5726c2719c5916ef
Tyler Rayls <raylst@oregonstate.edu>
2021-01-29 18:36:19 -0800

    Created components for main pages.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Navbar from './Navbar';
 
 const App = () => {
-	<Navbar />
+	return (
+		<Navbar />
+	);
 }
 export default App;
