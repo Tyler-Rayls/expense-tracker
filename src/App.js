598efb7c2306f0b3e21f759a40363a4d55b64f34
Tyler Rayls <raylst@oregonstate.edu>
2021-01-29 17:30:02 -0800

    Installed and imported react-router.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 const App = () => {
   return (
