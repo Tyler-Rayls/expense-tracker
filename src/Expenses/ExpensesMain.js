a81cae23693cb817fefec4751951ec1824a6547c
Tyler Rayls <raylst@oregonstate.edu>
2021-01-31 12:38:44 -0800

    Add expense form and expense table added to the main expense component

--- src/Expenses/ExpensesMain.js
+++ src/Expenses/ExpensesMain.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import AddExpense from './AddExpenseForm';
+import ExpenseTable from './ExpensesTable';
 
 const Expenses = () => {
     return (
         <div className="container">
             <h1 className="text-center">Expenses</h1>
+            <div className="row">
+                <AddExpense />
+            </div>
+            <div className="row">
+                <ExpenseTable />
+            </div>
         </div>
     );
 }
