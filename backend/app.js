8c57fc64b1a29bb6c3a6250b6797998be5c75642
Tyler Rayls <raylst@oregonstate.edu>
2021-02-19 07:44:50 -0800

    Added a route to test the backend

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,8 @@ const express = require("express");
 const app = express();
 const port = 4221;
 
+app.get("/express", (req, res) => {
+    res.send("Hello World!")
+});
+
+app.listen(port, () => console.log("Express is listening on the port ${port}"));
