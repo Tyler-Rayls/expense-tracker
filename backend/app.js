8925496aafe7cc795fecfb7e720483448f2b4fab
Tyler Rayls <raylst@oregonstate.edu>
2021-02-19 07:48:06 -0800

    Changed the testing text

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4221;
 
 app.get("/express", (req, res) => {
-    res.send("Hello World!")
+    res.send("Express is connected")
 });
 
 app.listen(port, () => console.log("Express is listening on the port ${port}"));
