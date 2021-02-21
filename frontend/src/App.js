03330edc1d74ff8bd9c5ad66e74f88dfc153656a
Tyler Rayls <raylst@oregonstate.edu>
2021-02-21 12:37:57 -0800

    Refactored app and navbar so the app handles routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import Navbar from './Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Login from './Login/Main';
+import Expenses from './Expenses/Main';
+import Family from './Families/Main';
+import Register from './Register/Main';
+import Index from './IndexOfFeatures';
+import AccountSettings from './ManageAccount/UserProfile';
+import CreditCardMain from './CreditCards/Main';
 
 const App = () => {
 	return (
-		<Navbar />
+		<Router>
+			<Navbar />
+
+			<Switch>
+				<Route path="/expenses">
+					<Expenses />
+				</Route>
+				<Route path="/family">
+					<Family />
+				</Route>
+				<Route path="/accountSettings">
+				<AccountSettings/>
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/features">
+					<Index />
+				</Route>
+				<Route path="/creditCards">
+					<CreditCardMain />
+				</Route>
+				<Route path="/">
+					<Login />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 export default App;
