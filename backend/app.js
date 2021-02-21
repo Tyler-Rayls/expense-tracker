ec1ee2e7bfb915bfd8cbe710fa98d6a57ae47bfe
Tyler Rayls <raylst@oregonstate.edu>
2021-02-21 11:28:01 -0800

    Add the insertion query for creating a new account

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require('cors');
+var mysql = require('./dbcon.js');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 const port = 4221;
 const corsOptions = {
     origin: "http://flip1.engr.oregonstate.edu:4220",
@@ -13,14 +13,23 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.set('mysql', mysql);
 
 app.get("/express", (req, res) => {
     res.send("Express is connected")
 });
 
 app.post("/register", (req, res) => {
-    console.log(req.body.firstName);
-    res.send(req.body);
+    var mysql = req.app.get('mysql');
+    var query = "INSERT INTO Users (email, firstName, lastName, password) VALUES (?, ?, ?, ?)";
+    var inserts = [req.body.email, req.body.firstName, req.body.lastName, req.body.password];
+    sql = mysql.pool.query(query, inserts, function(error, results, fields) {
+        if (error) {
+            res.send("Sorry, an account could not be created with this information");
+        } else {
+            res.send(`An account was created for ${req.body.firstName} using ${req.body.email}`);
+        }
+    })
 })
 
 app.listen(port, () => console.log(`Express is listening on the port ${port}`));
