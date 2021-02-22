d58db26870526463b137811086d55043814b9c16
Tyler Rayls <raylst@oregonstate.edu>
2021-02-21 20:40:36 -0800

    User storage access available everywhere needed

--- frontend/src/CreditCards/AddCardForm.js
+++ frontend/src/CreditCards/AddCardForm.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const AddExpenseForm = () => {
+    const user = useSelector((state) => state.user);
+    
     return(
         <>
             <div className="col-2">
