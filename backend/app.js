d3c64f05f5bcdec7910490f42e9f257a578723a1
Shane Yen <yensh@flip1.engr.oregonstate.edu>
2021-03-01 13:25:21 -0800

    Finished filter

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('mysql', mysql);
 
+//Add a new user to the Users table.
 app.post("/register", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "INSERT INTO Users (email, firstName, lastName, password) VALUES (?, ?, ?, ?)";
@@ -29,6 +30,7 @@ app.post("/register", (req, res) => {
     });
 });
 
+//Select a user from the Users table.
 app.get("/login", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "SELECT userID, email, firstName, lastName FROM Users WHERE email = ? AND password = ?";
@@ -48,6 +50,7 @@ app.get("/login", (req, res) => {
     });
 });
 
+//Select and display all credit cards in CreditCards.
 app.get("/creditcards", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "SELECT cardID, cardName, gas, grocery, travel, dining, otherReward, annualFee FROM CreditCards";
@@ -60,7 +63,7 @@ app.get("/creditcards", (req, res) => {
     });
 });
 
-
+//Add a credit card to the CreditCards table.
 app.post("/creditcards", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "INSERT INTO CreditCards (cardName, gas, grocery, travel, dining, otherReward, annualFee) VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -75,6 +78,7 @@ app.post("/creditcards", (req, res) => {
     });
 });
 
+//Select from CreditCards cards specific to the current user.
 app.post("/creditCardsForExpenseAndPaymentMethods", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "SELECT PaymentMethods.paymentID, CreditCards.cardID, CreditCards.cardName, CreditCards.gas, CreditCards.grocery, CreditCards.travel, CreditCards.dining, CreditCards.otherReward, CreditCards.annualFee FROM CreditCards \
@@ -91,6 +95,7 @@ app.post("/creditCardsForExpenseAndPaymentMethods", (req, res) => {
     });
 });
 
+//Select from PaymentMethods payment methods specific to the current user.
 app.post("/paymentMethods", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "SELECT paymentID, userID, cardID FROM PaymentMethods WHERE userID = ?";
@@ -105,6 +110,7 @@ app.post("/paymentMethods", (req, res) => {
     });
 });
 
+//Add a payment method to the PaymentMethods table.
 app.post("/addPaymentMethod", (req, res) => {
     var mysql = req.app.get('mysql');
     var sql = "INSERT INTO PaymentMethods (cardID, userID) VALUES (?, ?)";
@@ -119,132 +125,137 @@ app.post("/addPaymentMethod", (req, res) => {
     });
 });
 
-    app.post("/family", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var createFamily = "INSERT INTO Families (surname) VALUES (?)";
-        var inserts = [req.body.surname];
-        createFamily = mysql.pool.query(createFamily, inserts, function (error, results) {
-            if (error) {
-                var message = "There was an error creating this family.";
-                res.send({ message });
-            } else {
-                var addFamilyMember = "INSERT INTO FamilyMembers (userID, familyID, isHead) VALUES (?, ?, ?)";
-                var inserts = [req.body.user.userID, results.insertId, 1];
-                addFamilyMember = mysql.pool.query(addFamilyMember, inserts, function (error, results) {
-                    if (error) {
-                        var message = "There was an error creating this family and adding you as the head.";
-                    } else {
-                        var message = `Success! The ${req.body.surname} family was created with ${req.body.user.firstName} ${req.body.user.lastName} as the head of house.`;
-                    }
-                    res.send({ message });
-                });
-            }
-        })
-    });
-
-    app.get("/family", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var sql = "SELECT Families.familyID, Users.userID, Users.firstName, Users.lastName, Families.surname, FamilyMembers.isHead FROM Families \
-               INNER JOIN FamilyMembers ON Families.familyID = FamilyMembers.familyID \
-               INNER JOIN Users ON Users.userID = FamilyMembers.userID \
-               WHERE FamilyMembers.familyID IN (SELECT familyID from FamilyMembers WHERE userID = ?) \
-               ORDER BY Families.familyID, FamilyMembers.isHead DESC;";
-        var inserts = [req.query.userID];
-        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
-            var queryResults = [];
-            results.forEach((row) => {
-                queryResults.push(row)
-            });
-            res.send(queryResults);
-        });
-    });
-
-    app.post("/expensesTable", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
-        INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
-        INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
-        WHERE Expenses.userID = ?;";
-        insert = [req.body.userID]
-        sql = mysql.pool.query(sql, insert, function (error, results, fields) {
-            var queryResults = [];
-            results.forEach((row) => {
-                queryResults.push(row)
-            })
-            console.log(queryResults);
-            res.send(queryResults);
-        });
-    });
-
-
-    app.post("/addExpense", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO Expenses (userID, amount, date, category, paymentID) VALUES (?, ?, ?, ?, ?)";
-        var inserts = [req.body.userID, req.body.amount, req.body.date, req.body.category, req.body.paymentID];
-        sql = mysql.pool.query(sql, inserts, function (error, results) {
-            if (error) {
-                var message = "Error adding expense"
-            } else {
-                var message = "Expense was successfully added.";
-            }
-            res.send({ message });
-
-        });
-    });
-
-
-    app.post("/filterMonth", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
-        INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
-        INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
-        WHERE Expenses.userID = ? AND MONTHNAME(Expenses.date) = ?;";
-        insert = [req.body.userID, req.body.month]
-        sql = mysql.pool.query(sql, insert, function (error, results, fields) {
-            var queryResults = [];
-            results.forEach((row) => {
-                queryResults.push(row)
-            })
-            console.log(queryResults);
-            res.send(queryResults);
-        });
-    });
-
-    app.post("/filterCategory", (req, res) => {
-        var mysql = req.app.get('mysql');
-        var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
-        INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
-        INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
-        WHERE Expenses.userID = ? AND Expenses.category = ?;";
-        insert = [req.body.userID, req.body.category]
-        sql = mysql.pool.query(sql, insert, function (error, results, fields) {
-            var queryResults = [];
-            results.forEach((row) => {
-                queryResults.push(row)
-            })
-            console.log(queryResults);
-            res.send(queryResults);
-        });
-    });
-
-    
-
-    app.listen(port, () => console.log(`Express is listening on the port ${port}`));
-
-
-app.post("/expensesInsert", (req, res) => {
+//Add a new row into Families table
+app.post("/family", (req, res) => {
     var mysql = req.app.get('mysql');
-    var sql = "INSERT INTO Expenses (userID, amount, date, category, paymentID) VALUES (?, ?, ?, ?, ?)";
-    var inserts = [req.body.userID, req.amount, req.body.date, req.body.category, req.body.paymentID];
-    sql = mysql.pool.query(sql, inserts, function (error, results) {
+    var createFamily = "INSERT INTO Families (surname) VALUES (?)";
+    var inserts = [req.body.surname];
+    createFamily = mysql.pool.query(createFamily, inserts, function (error, results) {
         if (error) {
-            var message = "Error adding expense."
+            var message = "There was an error creating this family.";
+            res.send({ message });
         } else {
-            var message = "Expense added.";
+            var addFamilyMember = "INSERT INTO FamilyMembers (userID, familyID, isHead) VALUES (?, ?, ?)";
+            var inserts = [req.body.user.userID, results.insertId, 1];
+            addFamilyMember = mysql.pool.query(addFamilyMember, inserts, function (error, results) {
+                if (error) {
+                    var message = "There was an error creating this family and adding you as the head.";
+                } else {
+                    var message = `Success! The ${req.body.surname} family was created with ${req.body.user.firstName} ${req.body.user.lastName} as the head of house.`;
+                }
+                res.send({ message });
+            });
         }
-        res.send({ message });
+    })
+});
+
+//Select data from Families specific to the current user. 
+app.get("/family", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "SELECT Families.familyID, Users.userID, Users.firstName, Users.lastName, Families.surname, FamilyMembers.isHead FROM Families \
+            INNER JOIN FamilyMembers ON Families.familyID = FamilyMembers.familyID \
+            INNER JOIN Users ON Users.userID = FamilyMembers.userID \
+            WHERE FamilyMembers.familyID IN (SELECT familyID from FamilyMembers WHERE userID = ?) \
+            ORDER BY Families.familyID, FamilyMembers.isHead DESC;";
+    var inserts = [req.query.userID];
+    sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+        var queryResults = [];
+        results.forEach((row) => {
+            queryResults.push(row)
+        });
+        res.send(queryResults);
     });
 });
 
+//Display expenses from Expenses table specific to the current user.
+app.post("/expensesTable", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
+    INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
+    INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
+    WHERE Expenses.userID = ?;";
+    insert = [req.body.userID]
+    sql = mysql.pool.query(sql, insert, function (error, results, fields) {
+        var queryResults = [];
+        results.forEach((row) => {
+            queryResults.push(row)
+        })
+        console.log(queryResults);
+        res.send(queryResults);
+    });
+});
+
+//Add a new expense to the Expenses table.
+app.post("/addExpense", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO Expenses (userID, amount, date, category, paymentID) VALUES (?, ?, ?, ?, ?)";
+    var inserts = [req.body.userID, req.body.amount, req.body.date, req.body.category, req.body.paymentID];
+    sql = mysql.pool.query(sql, inserts, function (error, results) {
+        if (error) {
+            var message = "Error adding expense"
+        } else {
+            var message = "Expense was successfully added.";
+        }
+        res.send({ message });
+
+    });
+});
+
+//Display expenses from Expenses table specific to the current user and selected month filter.
+app.post("/filterMonth", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
+    INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
+    INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
+    WHERE Expenses.userID = ? AND MONTHNAME(Expenses.date) = ?;";
+    insert = [req.body.userID, req.body.month]
+    sql = mysql.pool.query(sql, insert, function (error, results, fields) {
+        var queryResults = [];
+        results.forEach((row) => {
+            queryResults.push(row)
+        })
+        console.log(queryResults);
+        res.send(queryResults);
+    });
+});
+
+//Display expenses from Expenses table specific to the current user and selected payment category filter.
+app.post("/filterCategory", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
+    INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
+    INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
+    WHERE Expenses.userID = ? AND Expenses.category = ?;";
+    insert = [req.body.userID, req.body.category]
+    sql = mysql.pool.query(sql, insert, function (error, results, fields) {
+        var queryResults = [];
+        results.forEach((row) => {
+            queryResults.push(row)
+        })
+        console.log(queryResults);
+        res.send(queryResults);
+    });
+});
+
+app.post("/filterCard", (req, res) => {
+    var mysql = req.app.get('mysql');
+    var sql = "SELECT PaymentMethods.paymentID, Expenses.userID, Expenses.amount, Expenses.date, Expenses.category, CreditCards.cardName FROM Expenses \
+    INNER JOIN PaymentMethods ON PaymentMethods.paymentID = Expenses.paymentID\
+    INNER JOIN CreditCards ON CreditCards.cardID = PaymentMethods.cardID\
+    WHERE Expenses.userID = ? AND CreditCards.cardName = ?;";
+    insert = [req.body.userID, req.body.cardName]
+    sql = mysql.pool.query(sql, insert, function (error, results, fields) {
+        var queryResults = [];
+        results.forEach((row) => {
+            queryResults.push(row)
+        })
+        console.log(queryResults);
+        res.send(queryResults);
+    });
+});
+
+
 app.listen(port, () => console.log(`Express is listening on the port ${port}`));
+
+
 
