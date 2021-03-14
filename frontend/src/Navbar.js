c57ae5b9aed5ed1f402966ea1bd2b6e2a7975fe0
Tyler Rayls <raylst@oregonstate.edu>
2021-03-14 12:22:26 -0700

    Home button links to account page if logged in

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -13,10 +13,12 @@ const Navbar = () => {
         currentUser = `Hello, ${user.firstName}!`
     }
 
+    const loggedIn = (user.userID == null) ? "/" : "accountSettings";
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand">Expense Tracker</Link>
+                <Link to={loggedIn} className="navbar-brand">Expense Tracker</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
