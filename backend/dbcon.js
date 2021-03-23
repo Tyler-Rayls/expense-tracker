b63722d7c8fdeff80e2ffc838d1c4e13f53d51ff
Tyler Rayls <62755797+Tyler-Rayls@users.noreply.github.com>
2021-03-23 11:30:37 -0400

    Update dbcon.js

--- backend/dbcon.js
+++ backend/dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_raylst',
-  password        : '4078',
-  database        : 'cs340_raylst'
+  host            : '',
+  user            : '',
+  password        : '',
+  database        : ''
 });
 module.exports.pool = pool;
